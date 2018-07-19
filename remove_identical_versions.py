7817b10eb6f9f35b4bc23bb88749d7c2ba16b33a
tstarr3 <tess.starr@trincolledu>
2018-07-19 17:14:28 -0400

    Merge branch 'master' of https://github.com/blackducksoftware/hub-rest-api-python
    # Conflicts:
    #	generate_config.py
    #	remove_empty_versions.py
    #	remove_identical_versions.py

--- remove_identical_versions.py
+++ remove_identical_versions.py
@@ -7,43 +7,57 @@ Compares adjacent versions in a project and removes older one if there are no ch
 
 '''
 from bds.HubRestApi import HubInstance
+import json
 
 # To clean up duplicates set cleanup = True
 
-cleanupEmpty = False
-cleanup = False
-
+cleanup = True
 
 hub = HubInstance()
 
-project = hub.get_project_by_id('17155144-404e-43b4-b5e4-d43efb31cbfc', limit=100)
+project = hub.get_project_by_id('6d93bd1c-eb01-4c94-b1c3-5a9c5865f10c', limit=100)
 
-
-print (project['name'])
+print(project['name'])
 versions = hub.get_project_versions(project, limit=200)
-print ("\t versions found %s" % versions['totalCount'])
+print("\t versions found %s" % versions['totalCount'])
 versionlist = versions['items']
-for index in range(len(versionlist) - 1):
-    va = versionlist[index]
-    print ("Processing {}".format(va['versionName']))
-    components = hub.get_version_components(va)
-    if cleanup and components['totalCount'] == 0:
-        print ("removing {}".format(va['_meta']['href']))
-        hub.execute_delete(va['_meta']['href'])
-        continue
-    vb = versionlist[index + 1]
-    result = hub.compare_project_versions(va, vb)
-    codelocations = hub.get_version_codelocations(va)
-    print ("comparing version {} to version {} total changes {}".format(
-        vb['versionName'], 
-        va['versionName'],
-        result['totalCount']))
-    print ("version {} has {} codelocations".format(va['versionName'], codelocations['totalCount']))
-    if cleanup and result['totalCount'] == 0:
-        print(hub.execute_delete(va['_meta']['href']))
-        for codelocation in codelocations['items']:
-            print (codelocation['_meta']['href'])
-            locationid = codelocation['_meta']['href'].split("/")[6]
-            print (locationid)
-            print (hub.delete_codelocation(locationid))
+attempt = 1
+index = 0
+while index < len(versionlist) - 1:
+    try:
+        va = versionlist[index]
+        print("Processing {}".format(va['versionName']))
+        components = hub.get_version_components(va)
+        if cleanup and components['totalCount'] == 0:
+            print("removing {}".format(va['_meta']['href']))
+            hub.execute_delete(va['_meta']['href'])
+            continue
+        vb = versionlist[index + 1]
+        result = hub.compare_project_versions(va, vb)
+        codelocations = hub.get_version_codelocations(va)
+        print("comparing version {} to version {} total changes {}".format(
+            vb['versionName'],
+            va['versionName'],
+            result['totalCount']))
+        print("version {} has {} codelocations".format(va['versionName'], codelocations['totalCount']))
+        if cleanup and result['totalCount'] == 0:
+            print(hub.execute_delete(va['_meta']['href']))
+            for codelocation in codelocations['items']:
+                print(codelocation['_meta']['href'])
+                locationid = codelocation['_meta']['href'].split("/")[6]
+                print(locationid)
+                print(hub.delete_codelocation(locationid))
 
+    except json.decoder.JSONDecodeError as json_decode_error:
+        print(json_decode_error)
+        if attempt < 3:
+            print("Trying again... this is attempt ", attempt+1)
+            index = index - 1
+            attempt = attempt + 1
+        else:
+            print("Too many attempts failed... moving on")
+            attempt = 1
+            print("---------------------------------------")
+            continue
+    finally:
+        index = index + 1
