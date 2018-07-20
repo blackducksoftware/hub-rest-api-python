f7e21f58f80f1fd5fddb6ad2ed615aff7282a578
tstarr3 <tess.starr@trincolledu>
2018-07-20 08:44:02 -0400

    Merge branch 'master' of https://github.com/blackducksoftware/hub-rest-api-python
    # Conflicts:
    #	generate_config.py
    #	remove_empty_versions.py
    #	remove_identical_versions.py

--- remove_identical_versions.py
+++ remove_identical_versions.py
@@ -9,7 +9,48 @@ Compares adjacent versions in a project and removes older one if there are no ch
 from bds.HubRestApi import HubInstance
 import json
 
-# To clean up duplicates set cleanup = True
+
+def clean_up(versionlist):
+    attempt = 1
+    index = 0
+    while index < len(versionlist) - 1:
+        try:
+            va = versionlist[index]
+            print("Processing {}".format(va['versionName']))
+            components = hub.get_version_components(va)
+            if cleanup and components['totalCount'] == 0:
+                print("removing {}".format(va['_meta']['href']))
+                hub.execute_delete(va['_meta']['href'])
+                continue
+            vb = versionlist[index + 1]
+            result = hub.compare_project_versions(va, vb)
+            codelocations = hub.get_version_codelocations(va)
+            print("comparing version {} to version {} total changes {}".format(
+                vb['versionName'],
+                va['versionName'],
+                result['totalCount']))
+            print("version {} has {} codelocations".format(va['versionName'], codelocations['totalCount']))
+            if cleanup and result['totalCount'] == 0:
+                print(hub.execute_delete(va['_meta']['href']))
+                for codelocation in codelocations['items']:
+                    print(codelocation['_meta']['href'])
+                    locationid = codelocation['_meta']['href'].split("/")[6]
+                    print(locationid)
+                    print(hub.delete_codelocation(locationid))
+
+        except json.decoder.JSONDecodeError as json_decode_error:
+            print(json_decode_error)
+            if attempt < 3:
+                print("Trying again... this is attempt ", attempt+1)
+                index -=1
+                attempt += 1
+            else:
+                print("Too many attempts failed... moving on")
+                attempt = 1
+                print("---------------------------------------")
+                continue
+        finally:
+            index += 1
 
 cleanup = True
 
@@ -21,43 +62,4 @@ print(project['name'])
 versions = hub.get_project_versions(project, limit=200)
 print("\t versions found %s" % versions['totalCount'])
 versionlist = versions['items']
-attempt = 1
-index = 0
-while index < len(versionlist) - 1:
-    try:
-        va = versionlist[index]
-        print("Processing {}".format(va['versionName']))
-        components = hub.get_version_components(va)
-        if cleanup and components['totalCount'] == 0:
-            print("removing {}".format(va['_meta']['href']))
-            hub.execute_delete(va['_meta']['href'])
-            continue
-        vb = versionlist[index + 1]
-        result = hub.compare_project_versions(va, vb)
-        codelocations = hub.get_version_codelocations(va)
-        print("comparing version {} to version {} total changes {}".format(
-            vb['versionName'],
-            va['versionName'],
-            result['totalCount']))
-        print("version {} has {} codelocations".format(va['versionName'], codelocations['totalCount']))
-        if cleanup and result['totalCount'] == 0:
-            print(hub.execute_delete(va['_meta']['href']))
-            for codelocation in codelocations['items']:
-                print(codelocation['_meta']['href'])
-                locationid = codelocation['_meta']['href'].split("/")[6]
-                print(locationid)
-                print(hub.delete_codelocation(locationid))
-
-    except json.decoder.JSONDecodeError as json_decode_error:
-        print(json_decode_error)
-        if attempt < 3:
-            print("Trying again... this is attempt ", attempt+1)
-            index = index - 1
-            attempt = attempt + 1
-        else:
-            print("Too many attempts failed... moving on")
-            attempt = 1
-            print("---------------------------------------")
-            continue
-    finally:
-        index = index + 1
+clean_up(versionlist)
