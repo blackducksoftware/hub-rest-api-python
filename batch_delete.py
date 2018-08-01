443083b002699b50b7f42fb5fbae2781554fff2f
Murat Kumykov <mkumykov@blackducksoftware.com>
2018-08-01 10:15:21 -0400

    Execute batch delete

---
+++ batch_delete.py
@@ -0,0 +1,30 @@
+'''
+Created on Aug 1, 2018
+
+@author: kumykov
+
+Execute batch request  
+'''
+from sys import argv,exit
+from bds.HubRestApi import HubInstance
+
+def print_usage():
+    print("\n\n\t python3 {} urllist ".format(argv[0]))
+
+
+#
+# main
+# 
+
+if (len(argv) < 2):
+    print_usage()
+    exit()
+
+with open(argv[1], "r") as f:
+    urllist = f.readlines()
+     
+hub = HubInstance()
+
+for url in urllist:
+    print ("Processing {} ".format(url))
+    print(hub.execute_delete(url))
