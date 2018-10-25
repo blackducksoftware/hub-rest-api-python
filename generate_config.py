95e94c8b77a0cc9f038a63249ecfe06d03feac66
Murat Kumykov <mkumykov@blackducksoftware.com>
2018-10-25 12:05:31 -0400

    .

--- generate_config.py
+++ generate_config.py
@@ -5,4 +5,4 @@ Created on Jul 18, 2018
 '''
 from bds.HubRestApi import HubInstance
 
-hub = HubInstance("https://ec2-18-208-209-223.compute-1.amazonaws.com","sysadmin","genesys", insecure=True, debug=True)
+hub = HubInstance("https://ec2-18-214-36-48.compute-1.amazonaws.com","sysadmin","blackduck", insecure=True, debug=True)
