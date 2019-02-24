03ddd6c0fb22d3943644c4098e242e1dbeb48a35
Glenn Snyder <gsnyder@blackducksoftware.com>
2019-02-24 15:14:51 -0500

    adding support for retrieving license info for bom components; also fixing an issue with retrieving file matches for bom components

--- blackduck/HubRestApi.py
+++ blackduck/HubRestApi.py
@@ -546,6 +546,41 @@ class HubInstance(object):
 
     ##
     #
+    # License stuff
+    #
+    ##
+    def _get_license_info(self, license_obj):
+        if 'license' in license_obj:
+            license_info = {}
+            text_json = {}
+            logging.debug("license: {}".format(license_obj))
+            response = self.execute_get(license_obj['license'])
+            if response.status_code == 200:
+                license_info = response.json()
+                text_url = self.get_link(license_info, 'text')
+                response = self.execute_get(text_url)
+                if response.status_code == 200:
+                    text_json = response.text
+            yield {"license_info": license_info,
+                    "license_text_info": text_json}
+        elif 'licenses' in license_obj and isinstance(license_obj['licenses'], list):
+            for license in license_obj['licenses']:
+                self._get_license_info(license)
+
+    def get_license_info_for_bom_component(self, bom_component, limit=1000):
+        self._check_version_compatibility()
+        all_licenses = {}
+        logging.debug("gathering license info for bom component {}, version {}".format(
+            bom_component['componentName'], bom_component['componentVersionName']))
+        for license in bom_component.get('licenses', []):
+            for license_info_obj in self._get_license_info(license):
+                all_licenses.update({
+                        license['licenseDisplay']: license_info_obj
+                    })
+        return all_licenses
+
+    ##
+    #
     # Files and Snippet matching
     #
     ##
@@ -564,11 +599,10 @@ class HubInstance(object):
         jsondata = response.json()
         return jsondata
 
-    def get_file_matches_for_component_with_version(self, project_id, version_id, component_id, component_version_id, limit=1000):
+    def get_file_matches_for_bom_component(self, bom_component, limit=1000):
         self._check_version_compatibility()
+        url = self.get_link(bom_component, "matched-files")
         paramstring = self.get_limit_paramstring(limit)
-        url =  "{}/projects/{}/versions/{}/components/{}/versions/{}/matched-files".format(
-            self.get_apibase(), project_id, version_id, component_id, component_version_id)
         logging.debug("GET {}".format(url))
         response = self.execute_get(url)
         jsondata = response.json()
--- blackduck/__version__.py
+++ blackduck/__version__.py
@@ -1,4 +1,4 @@
 
-VERSION = (0, 0, 12)
+VERSION = (0, 0, 13)
 
 __version__ = '.'.join(map(str, VERSION))
