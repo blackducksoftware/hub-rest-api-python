1fd6a14b9f82c2c5a01644c7a60921e436279a03
Glenn Snyder <gsnyder@blackducksoftware.com>
2020-08-28 06:58:59 -0400

    converting tabs to spaces, ugh

--- examples/get_licenses.py
+++ examples/get_licenses.py
@@ -23,10 +23,10 @@ hub = HubInstance()
 parameters = {}
 
 if args.keyword:
-	parameters.update({"q": f"name:{args.keyword}"})
+    parameters.update({"q": f"name:{args.keyword}"})
 
 if args.limit:
-	parameters.update({"limit": args.limit})
+    parameters.update({"limit": args.limit})
 
 logging.debug(f"Retrieving licenses using parameters={parameters}")
 licenses = hub.get_licenses(parameters=parameters).get('items', [])
@@ -34,14 +34,14 @@ licenses = hub.get_licenses(parameters=parameters).get('items', [])
 logging.debug(f"Found {len(licenses)} licenses")
 
 for license in licenses:
-	logging.debug(f"Retrieving license text, terms, and obligations info for license \"{license['name']}\"")
-	text_url = hub.get_link(license, "text")
-	license['text'] = hub.execute_get(text_url).json().get('text')
+    logging.debug(f"Retrieving license text, terms, and obligations info for license \"{license['name']}\"")
+    text_url = hub.get_link(license, "text")
+    license['text'] = hub.execute_get(text_url).json().get('text')
 
-	terms_url = hub.get_link(license, 'license-terms')
-	license['terms'] = hub.execute_get(terms_url).json().get('items', [])
+    terms_url = hub.get_link(license, 'license-terms')
+    license['terms'] = hub.execute_get(terms_url).json().get('items', [])
 
-	obligations_url = hub.get_link(license, 'license-obligations')
-	license['obligations'] = hub.execute_get(obligations_url).json().get('items', [])
+    obligations_url = hub.get_link(license, 'license-obligations')
+    license['obligations'] = hub.execute_get(obligations_url).json().get('items', [])
 
 print(json.dumps(licenses))
