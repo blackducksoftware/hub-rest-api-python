7568526695fc891df0d09fd47d1a3626a605fe58
kumykov <kumykov@synopsys.com>
2021-01-15 15:32:58 -0500

    Internal call for scan status count added

--- examples/read_scan_status.py
+++ examples/read_scan_status.py
@@ -16,22 +16,12 @@ batch_size=500
 codelocations = hub.get_codelocations(limit=1, offset=1)
 total_count = codelocations['totalCount']
 
-print("{} codelocations in the database".format(total_count))
-#print(json.dumps(codelocations, indent=2))
+status = {"in_progress": 0, "unstarted": 0, "completed": 0, "error": 0, "skipped": 0}
 
-scan_status = {}
+for i in status.keys():
+    codelocations = hub.get_codelocations_internal(limit=1, offset=0, parameters={"filter": "codeLocationStatus:{}".format(i)})
+    status[i] = codelocations['totalCount']
+    
+print (status)
 
-offset=0
-while offset < total_count:
-    hub = HubInstance()
-    codelocations = hub.get_codelocations(limit=batch_size, offset=offset)
-    for codelocation in codelocations['items']:
-        scan_summaries = hub.get_codelocation_scan_summaries(code_location_obj=codelocation)
-        for item in scan_summaries['items']:
-            status = item['status']
-            if status in scan_status.keys():
-                scan_status[status] += 1
-            else:
-                scan_status[status] = 1
-    offset += batch_size
-    print (json.dumps(scan_status, indent=2))
+
