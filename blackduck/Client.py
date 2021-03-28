ea59c544894dfbac66c34e501dbc8a54e17c5fe1
Glenn Snyder <gsnyder@synopsys.com>
2021-03-28 12:35:17 -0400

    refactoring

--- blackduck/Client.py
+++ blackduck/Client.py
@@ -14,9 +14,10 @@ import requests
 logger = logging.getLogger(__name__)
 
 class Client:
-    VERSION_DISTRIBUTION=["EXTERNAL", "SAAS", "INTERNAL", "OPENSOURCE"]
-    VERSION_PHASES = ["PLANNING", "DEVELOPMENT", "PRERELEASE", "RELEASED", "DEPRECATED", "ARCHIVED"]
-    PROJECT_VERSION_SETTINGS = ['nickname', 'releaseComments', 'versionName', 'phase', 'distribution', 'releasedOn']
+    '''
+    classdocs
+    '''
+    from .constants import VERSION_DISTRIBUTION, VERSION_PHASES, PROJECT_VERSION_SETTINGS
 
     from .Exceptions import(
         http_exception_handler
