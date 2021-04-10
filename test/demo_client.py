076d47d4ea5e675e0f9e195ed57f63166a34a377
Andrew Calder <androbcal@gmail.com>
2021-04-11 00:42:14 +0100

    update Client demo examples to use new get_resource

--- test/demo_client.py
+++ test/demo_client.py
@@ -11,27 +11,26 @@ from datetime import datetime, timedelta
 import blackduck
 
 def vulns_in_all_project_versions_components(bd):
-    for project in bd.get_projects():
+    for project in bd.get_resource(name='projects'):
         for version in bd.get_resource(project, 'versions'):
             for component in bd.get_resource(version, 'components'):
                 for vulnerability in bd.get_resource(component, 'vulnerabilities'):
                     print(f"{project.get('name')}-{version.get('versionName')} [{component.get('componentName')}] has {vulnerability.get('severity')} severity vulnerability '{vulnerability.get('name')}'")
     
 def list_project_subresources(bd):
-    for project in bd.get_projects():
+    for project in bd.get_resource(name='projects'):
         subresources = bd.list_resources(project)
         print(f"projects has the following subresources: {', '.join(subresources)}")
         return
 
 def list_project_versions(bd):
-    projects = list(bd.get_projects(bd))
-    num_projects = len(projects)
-    i = 0
-    for project in projects:
-        i += 1
-        print(f"Project ({i}/{num_projects}): {project.get('name')}")
+    i = 1
+    project_count = bd.get_metadata(name='projects').get('totalCount')
+    for project in bd.get_resource(name='projects'):
+        print(f"Project ({i}/{project_count}): {project.get('name')}")
         for version in bd.get_resource(project, 'versions'):
             print(f"  {version.get('versionName')}")
+        i += 1
 
 def projects_added_at_4_week_intervals(bd):
     last_count = 0
@@ -40,20 +39,20 @@ def projects_added_at_4_week_intervals(bd):
     for timestamp in blackduck.Utils.iso8601_timespan(days_ago=365, delta=timedelta(weeks=4)):
         last_count=count
         count=0
-        for project in bd.get_projects():
+        for project in bd.get_resource(name='projects'):
             created_at = blackduck.Utils.iso8601_to_date(project.get('createdAt'))
             count += (created_at <= blackduck.Utils.iso8601_to_date(timestamp))
 
-        print(f"{count-last_count} projects as of {timestamp}")
+        print(f"{count-last_count} projects added as of {timestamp}")
 
 bd = blackduck.Client(
-    token=os.environ.get('blackduck_token', 'YOUR TOKEN HERE'),
-    base_url="https://your.blackduck.url",
-    # verify=False  # TLS certificate verification
+    token=os.environ.get('blackduck_token', 'MISSING_ENV_VAR'),
+    base_url=os.environ.get('blackduck_url', 'MISSING_ENV_VAR'),
+    verify=False  # TLS certificate verification
 )
 
 # Various examples:
-#vulns_in_all_project_versions_components(bd)
-#projects_added_at_4_week_intervals(bd)
+# vulns_in_all_project_versions_components(bd)
+projects_added_at_4_week_intervals(bd)
 list_project_subresources(bd)
 list_project_versions(bd)
