347ddc6204a56ffcdb1221856aa004bcba0a67bb
EC2 Default User <ec2-user@ip-172-31-13-171.us-east-2.compute.internal>
2021-12-02 03:44:38 +0000

    Total rewrite to improve peformance and fit customer workflow.
    * Renamed vars & functions to improve readability. Also reorganized
      functions and added whitespace to improve readability.
    * Improved performance by sorting components on the server and
      consuming through generators rather than slurping them all at once.
    * Added limits when searching for copyrights in older components. If
      you can't find a copyright in the last 10 versions, your're probably
      never going to find them.
    * Added filtering to remove duplicate copyrights (after normalizing
      whitespace and lettercase).
    * Format output to match the existing Notices report, so it has a
      familiar human-readable appearance.
    * Added exception handling to cover connection failures, unexpected
      data structures from the API, and other edge cases.

--- examples/bd-upstream-copyrights/get_upstream_copyrights.py
+++ examples/bd-upstream-copyrights/get_upstream_copyrights.py
@@ -28,7 +28,7 @@ under the License.
 This program will attempt to produce missing Copyright statements 
 for BOM entries that do not have orgin associated copyright statements.
 
-The scriot will enumerate BOM entried that appear on NOTICES report 
+The script will enumerate BOM entried that appear on NOTICES report 
 without copyright and for each of then will try to retrieve 
 upstream copyright notices from source origin (github, long_tail)
 
@@ -38,37 +38,18 @@ anf try to fetch copyright statements from there.
 Note:  There is still a possibility that there will be no copyright 
        statements present on any version of a component
 
-Once completed a text file will be writtent with the following format:
-Optional JSON file can be written
-
-#####
-ComponentName ComponentversionName
-#####
------
-Statement-1
------
-Statement-1
------
-.....
-
 Usage: Enumerate BOM componets without copyrigth statements. retrieve 
        copyright statements form upstream channel and/or version
 
 python3 get_upstream_copyrights.py [OPTIONS]
 
 OPTIONS:
-    [-h]                            Help
+    -h                              Show help
     -u BASE_URL                     URL of a Blackduck system
     -t TOKEN_FILE                   Authentication token file
     -p PROJECT_NAME                 Project to process
     -v VERSION_NAME                 Project Version to process
-    [-o OUTPUT_FILE]                Output file (default: copyright_data.txt)
-    [-jo JSON_OUTPUT_FILE]          Write an optional JSON file with output data
-    [-ukc USE_UPDATED_COPYRIGHT]    Use kbCopyright instead of updatedCopyright (default: false)
-    [-nv]                           Trust TLS certificate
-
-
-
+    -nv                             Trust TLS certificate
 
 '''
 
@@ -76,19 +57,66 @@ import argparse
 import json
 import logging
 import sys
-from datetime import timedelta
-from pprint import pprint
-
 import arrow
+
+from itertools import islice
+from datetime  import timedelta
 from blackduck import Client
-from blackduck.Utils import get_resource_name
 
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', stream=sys.stderr, level=logging.DEBUG)
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("blackduck").setLevel(logging.DEBUG)
 
+#----------------------------------------------------------------------------
+
+def main():
+
+    args = parse_command_args()
+    components_with_no_copyrights = []
+
+    with open(args.token_file, 'r') as tf:
+        access_token = tf.readline().strip()
+
+    global bd
+    bd = Client(base_url=args.base_url, token=access_token, verify=args.no_verify, timeout=60.0, retries=4)
+    project_name, project_version = get_project_version_by_name(args.project_name, args.version_name)
+
+    for component in bd.get_resource("components", project_version):
+        try:
+            copyrights = find_copyrights_in_bom_component_origins(component)           \
+                      or find_copyrights_in_github_origins(component)                  \
+                      or find_copyrights_in_previous_component_versions(component)
+
+            if copyrights:
+                print_copyrights(component, copyrights)
+            else:
+                components_with_no_copyrights.append(component)
+
+        except Exception as e:
+            logging.debug(f"Component {component['componentName']} failed with {e}")
+
+    if args.report_missing:
+        report_components_with_no_copyrights(components_with_no_copyrights)
+
+#----------------------------------------------------------------------------
+
+def parse_command_args():
+
+    parser = argparse.ArgumentParser("Print copyrights for BOM using upstream origin or prior version if not available.")
+    parser.add_argument("-u", "--base-url",     required=True, help="Hub server URL e.g. https://your.blackduck.url")
+    parser.add_argument("-t", "--token-file",   required=True, help="File containing access token")
+    parser.add_argument("-p", "--project-name", required=True, help="Project Name")
+    parser.add_argument("-v", "--version-name", required=True, help="Version Name")
+    parser.add_argument("-r", "--report-missing", action='store_true',  help="Report components with no copyrights")
+    parser.add_argument("-nv", "--no-verify",     action='store_false', help="Disable TLS certificate verification")
+
+    return parser.parse_args()
+
+#----------------------------------------------------------------------------
+
 def get_project_version_by_name(project_name, version_name):
+
     params = {
         'q': [f"name:{project_name}"]
     }
@@ -104,123 +132,106 @@ def get_project_version_by_name(project_name, version_name):
     version = versions[0]
     return project, version
 
-def find_copyright_for_component(component):
-    # Search for copyrights associated with github origin. 
-    # If present, search stops here
-    if component.get('origins',None):
-        copyright_records = find_github_copyrights(component)    
-        if len(copyright_records) > 0:
-            return copyright_records
-    
-    # Continue searching in previous versions of a component
-    # in reverse chronological order.
-    # Skip versions that are newer that one in the BOM
-    copyright_records = find_copyrights_in_previous_versions(component)
-    return copyright_records
+#----------------------------------------------------------------------------
 
-def find_copyrights_in_previous_versions(component):
-    release_date = component.get('releasedOn', None)
-    component_versions = [version for version in bd.get_resource('versions', bd.get_json(component['component']))]
-    copyright_records = []
-    for version in sorted(component_versions, key= lambda version: version['releasedOn'], reverse = True) :
-        version_release_date = version['releasedOn']
-        diff = arrow.get(release_date) - arrow.get(version_release_date)
-        if diff < timedelta(0):
-            logging.debug(f"Version {version['versionName']} is newer, skipping ...")
-            continue
-        for origin in bd.get_resource('origins', version):
-            origin_copyrights = bd.get_resource('component-origin-copyrights', origin)
-            copyright_records += [copyright for copyright in origin_copyrights]
-        logging.debug(f"Found {len(copyright_records)} for {component['componentName']} {version['versionName']}")
-        if len(copyright_records) > 0:
-            break
-    return copyright_records
+def find_copyrights_in_bom_component_origins(component):
 
-def find_github_copyrights(component):
-    origins = bd.get_resource('origins', component)
-    github_origins = list(filter(
-        lambda origin: origin.get('originName','unknown') == 'github' or origin.get('originName','unknown') == 'long_tail', origins))
-    github_copyrights = list()
+    copyrights = []
+    for origin in component['origins']:
+        copyrights += list(bd.get_resource('component-origin-copyrights', origin))
+
+    return copyrights
+
+#----------------------------------------------------------------------------
+
+def find_copyrights_in_github_origins(component):
+
+    if not component.get('origins'):
+        return []
+
+    def origin_is_lambda_or_long_tail(origin):
+        origin_name = origin.get('originName')
+        return origin_name == 'github' or origin_name == 'long_tail'
+
+    all_origins = list(bd.get_resource('origins', component))
+    github_origins = filter(origin_is_lambda_or_long_tail, all_origins)
+
+    copyrights = []
     for origin in github_origins:
-        github_copyrights += [copyright for copyright in bd.get_resource('component-origin-copyrights',origin) if copyright['active']]
-    return github_copyrights
+        copyrights += list(bd.get_resource('component-origin-copyrights',origin))
 
-def write_text_output(output_data, output_file, usekb_copyright=False):
-    f = open(output_file, "w")
-    hashes = "#"*80
-    dashes = "-"*80
-    for entry in output_data:
-        f.write(f"{hashes}\n")
-        f.write(f"{entry['component']['componentName']} {entry['component'].get('componentVersionName', None)}")
-        f.write(f"\n{hashes}\n{dashes}\n")
-        for copyright in entry['copyrights']:
-            if usekb_copyright:
-                f.write(copyright['kbCopyright'])
-            else:
-                f.write(copyright['updatedCopyright'])
-            f.write(f"\n{dashes}\n")
-        f.write("\n\n")
-    f.write("\n")
-    f.close()
+    return copyrights
 
-def main():
-    upstream_copyright_data_file='copyright_data.txt'
+#----------------------------------------------------------------------------
 
-    parser = argparse.ArgumentParser("Enumerate BOM componets without copyrigth statements. retrieve copyright statements form upstream channel and/or version")
-    parser.add_argument("-u", "--base-url", required=True, help="Hub server URL e.g. https://your.blackduck.url")
-    parser.add_argument("-t", "--token-file", dest='token_file', required=True, help="containing access token")
-    parser.add_argument("-p", "--project-name", dest='project_name', required=True, help="Project Name")
-    parser.add_argument("-v", "--version-name", dest='version_name', required=True, help="Version Name")
-    parser.add_argument("-o", "--output-file", dest='output_file', default=upstream_copyright_data_file, help=f"TEXT-formatted file with data")
-    parser.add_argument("-jo", "--json-output-file", dest='json_output_file', default=None, help='JSON formatted file with data')
-    parser.add_argument("-ukc", "--use-kb-copyright", dest='use_kb_copyright', default=False, help='Use KB Copyright vs Updated Copyright')
-    parser.add_argument("-nv", "--no-verify", dest='verify', action='store_false', help="disable TLS certificate verification")
-    args = parser.parse_args()
+def find_copyrights_in_previous_component_versions(component):
+    
+    params = {'sort' : 'releasedOn DESC'}
+    component_json = bd.get_json(component['component'])
+    all_component_versions_sorted_by_release_date = bd.get_resource('versions', component_json, params=params)
 
-    with open(args.token_file, 'r') as tf:
-        access_token = tf.readline().strip()
+    copyrights = []
+    release_date = component.get('releasedOn')
 
-    global bd
-    bd = Client(base_url=args.base_url, token=access_token, verify=args.verify, timeout = 60.0)
+    component_count = 0
+    for component_version in all_component_versions_sorted_by_release_date:
 
-    project, version = get_project_version_by_name(args.project_name, args.version_name)
-    logging.debug(f"Found {project['name']}:{version['versionName']}")
+            # Skip component versions that are newer than the one we are trying to find copyrights for.
+            # This code doesn't currently work because the release_date for a BOM component version
+            # is the date of the scan that detected the component. This is probably a bug in the API.
+            # diff = arrow.get(release_date) - arrow.get(component_version['releasedOn'])
+            # if diff < timedelta(0): continue
 
-    # retrieve bill of materials and locate entries without copyrights
-    bom_components_with_no_copyrights = []
-    for component in bd.get_resource("components", version):
-        logging.debug(f"Processing component {component['componentName']}")
-        #if component['componentName'] != 'musl':
-        #    continue
-        origins = component['origins']
-        if len(origins) == 0:
-            bom_components_with_no_copyrights.append(component)
-            continue
-        copyrights_count = 0
-        for origin in origins:
-            logging.debug(f"Processing origin {origin['externalNamespace']}  {origin['externalId']}")
-            origin_copyrights = [copyright for copyright in bd.get_resource('component-origin-copyrights', origin)]
-            copyrights_count += len(origin_copyrights)
-            if copyrights_count > 0:
+            component_count += 1
+            if component_count > 10:
                 break
-        logging.debug(f"Copyright count = {copyrights_count}")
-        if copyrights_count == 0:
-            bom_components_with_no_copyrights.append(component)
 
-    logging.debug(f"Found {len(bom_components_with_no_copyrights)} components with no copyright")
-    output_data = list()
-    for component in bom_components_with_no_copyrights:
-        logging.debug(f"Locating copyrights for component {component['componentName']} {component.get('componentVersionName', None)}")
-        copyrights = find_copyright_for_component(component)
-        logging.debug(f"Found {len(copyrights)} active records")
-        output_data.append ({'component': component, 'copyrights': copyrights })
+            origin_count = 0
+            for origin in bd.get_resource('origins', component_version):
 
-    logging.debug(f"Writing results into {args.output_file} file")
-    write_text_output(output_data, args.output_file, args.use_kb_copyright)
-    if args.json_output_file:
-        logging.debug(f"Writing results into {args.json_output_file} file")
-        with open(args.json_output_file, 'w') as f:
-            json.dump(output_data, f, indent=2)
-        
+                origin_count += 1
+                if copyrights or origin_count > 10:
+                    break
+
+                copyrights = list(bd.get_resource('component-origin-copyrights', origin))
+
+    return copyrights
+
+#----------------------------------------------------------------------------
+
+def print_copyrights(component, copyrights):
+
+    print(f"\n{component.get('componentName')} {component.get('componentVersionName')}")
+    already_printed_copyrights = {}
+
+    for copyright in copyrights:
+
+        # Everything after the first newline is usually junk. So we print only the
+        # first line of any copyright statement. Also, we skip any copyright statement
+        # that is an exact duplicate or differs only by whitespace or lettercase from
+        # one that we've already printed.
+
+        first_line_of_copyright_statement = copyright['updatedCopyright'].splitlines()[0]
+        copyright_without_whitespace = "".join(first_line_of_copyright_statement.split()).lower()
+
+        if copyright_without_whitespace in already_printed_copyrights:
+            continue
+
+        already_printed_copyrights[copyright_without_whitespace] = None
+        print(f"     {first_line_of_copyright_statement}")
+
+#----------------------------------------------------------------------------
+
+def report_components_with_no_copyrights(components):
+
+    print("=" * 80)
+    print("No copyrights found for these components")
+    for component in components:
+        print(f"{component.get('componentName')} {component.get('componentVersionName')}")
+
+    print(f"Total: {len(components)}")
+
+#----------------------------------------------------------------------------
+
 if __name__ == "__main__":
     sys.exit(main())
