6b50f9e90c89e210f8c084e43833ddb316dc5733
Murat Kumykov <kumykov@synopsys.com>
2024-02-09 10:22:33 -0500

    excel input test

--- examples/client/multi-image/generate-clone.sh
+++ examples/client/multi-image/generate-clone.sh
@@ -9,7 +9,12 @@ le:testcontainer:2.4,\
 login-app-ui:testcontainer:2.4,\
 nlv:testcontainer:2.4"
 
+SPECFILE=~/Documents/Ciena/excelparameters/BP_SampleProduct.xlsx
+
+ls -l $SPECFILE
+
 COMMAND="python3 examples/client/multi-image/manage_project_structure.py"
 
-$COMMAND -u $BD_URL -t token -nv -pg "Test Group" -p P3 -pv 2.4 -sp $SUBPROJECTS --clone-from 2.3 $@
+# $COMMAND -u $BD_URL -t token -nv -pg "Test Group" -p P3 -pv 2.4 -sp $SUBPROJECTS --clone-from 2.3 $@
+$COMMAND -u $BD_URL -t token -nv -pg "Test Group" -p P3 -pv 2.4 -ssf $SPECFILE --clone-from 2.3 $@
 
